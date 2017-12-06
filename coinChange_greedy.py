77cd1142e590778ed2964c0be8e8c6b34aad4f7c
Gesa Behrends <gesa.behrends@googlemail.com>
2017-12-06 13:00:33 +0100

    Compare greedy to dynamic in money change problem

---
+++ coinChange_greedy.py
@@ -0,0 +1,74 @@
+
+
+def coinChangeGreedy(money, coins):
+    n = len(coins)
+    returnCoins = []
+    while( n > 0):
+        n -= 1
+        returnCoins.append(money//coins[n])
+        money = money%coins[n]
+    return sum(returnCoins)
+
+def coinChangeDynamic(money, coins):
+
+    matrix = [list(range(0,money + 1))]
+
+    def coinChangeHelper(money, coins, matrix):
+
+        for i in range(1, len(coins)):
+            listForCoin = []
+            for j in range(money+1):
+                if (j) < coins[i]:
+                    listForCoin.append(matrix[i - 1][j])
+                elif j == coins[i]:
+                    listForCoin.append(1)
+                else: 
+                    possibilities = []
+                    for k in range((j // coins[i])+1):
+                        value = matrix[i-1][j-k*coins[i]] + k
+                        #print("Um", j, "Geld zu wechseln, nutzen wir", k, " mal die Muenze", coins[i])
+                        #print(j-k*coins[i], "weniger angucken")
+                        #print("Hierfuer werden Muenzen gebraucht: ", matrix[i-1][j-k*coins[i]])
+                        #print("zusammen benoetigt man Muenzen: ", value)
+                        possibilities.append(value)
+                    listForCoin.append(min(possibilities))
+            matrix.append(listForCoin)
+        return matrix
+
+    matrix = (coinChangeHelper(money, coins, matrix))
+    return matrix[len(coins)-1][money]
+
+
+def compareGreedyToDynamic(coins):
+
+    funktioniert = True
+    print(calculatekgV(coins))
+    for i in range(1, calculatekgV(coins)):
+        if( coinChangeDynamic(i, coins) == coinChangeGreedy(i, coins)):
+            funktioniert &= True
+        else:
+            funktioniert &= False
+    return funktioniert
+
+def calculatekgV(coins):
+    def ggT(m,n):
+        if (n==0):
+            return m
+        else:
+            return ggT(n, m%n)
+
+    def kgV(m,n):
+        o = ggT(m,n)
+        p = (m * n) // o
+        return p
+
+    if len(coins) < 1: return 1
+    if len(coins) == 2:
+        return kgV(coins[0], coins[1])
+    else:
+        l = 1
+        for i in range(len(coins)):
+            l = kgV(l, coins[i])
+        return l
+
+
